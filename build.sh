dd85802b7b8e4a50beca8660971baf58158e69eb
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2023-08-08 17:07:24 -0700

    Edit script to only backup files if home-manager build is successful

--- build.sh
+++ build.sh
@@ -11,6 +11,18 @@ fi
 # Remove the symlink result dir
 rm "$HM/result" >/dev/null 2>&1
 
+# Build config
+home-manager build
+
+# If result symlink is not found then exit
+# otherwise activate config
+if ! [ -L "result" ]; then
+    exit 1
+fi
+
+# Run script to install necessary packages
+bash ./scripts/install_pkgs.sh
+
 # Files that will be manipulated
 files=(
     # VSCode files
@@ -24,17 +36,8 @@ do
     backupfile $file
 done
 
-# Build config
-home-manager build
-
-# If result symlink is not found then exit
-# otherwise activate config
-if ! [ -L "result" ]; then
-    exit 1
-else
-    bash ./scripts/install_pkgs.sh
-    $HM/result/activate
-fi
+# Activate script
+$HM/result/activate
 
 # Turn the symlinks into regular files
 for file in "${files[@]}"
