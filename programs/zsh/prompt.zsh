94a6ca95c1842da51b9349f1d9fd8266525f9b12
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2023-08-09 21:08:19 -0700

    Add more to zsh with nix

--- programs/zsh/prompt.zsh
+++ programs/zsh/prompt.zsh
@@ -1,8 +1,38 @@
-zsh_dir="$HOME/.config/home-manager/programs/zsh";
+zsh_dir="/home/user/.config/home-manager/programs/zsh";
 
-PROMPT=$'\n┏╸%B%~%b $(gitprompt)\n' \
-&& PROMPT+='┗╸%U${CONDA_DEFAULT_ENV}%u%(!.%F{1}.%F{238})' \
-&& PROMPT+='[%n@%M]%f%(!.#.$) '
+alias src="source ~/.zshrc"
 
 source ${zsh_dir}/settings.zsh
 source ${zsh_dir}/aliases.zsh
+
+# Theming variables for primary prompt
+# ZSH_THEME_GIT_PROMPT_PREFIX=""
+# ZSH_THEME_GIT_PROMPT_SUFFIX=""
+# ZSH_THEME_GIT_PROMPT_SEPARATOR=" "
+# ZSH_THEME_GIT_PROMPT_DETACHED="%{$fg_bold[yellow]%}:"
+# ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
+# ZSH_THEME_GIT_PROMPT_UPSTREAM_SYMBOL="%{$fg_bold[yellow]%}⟳ "
+# ZSH_THEME_GIT_PROMPT_UPSTREAM_NO_TRACKING="%{$fg_bold[red]%}!"
+# ZSH_THEME_GIT_PROMPT_UPSTREAM_PREFIX="%{$fg[red]%}(%{$fg[yellow]%}"
+# ZSH_THEME_GIT_PROMPT_UPSTREAM_SUFFIX="%{$fg[red]%})"
+# ZSH_THEME_GIT_PROMPT_BEHIND="↓"
+# ZSH_THEME_GIT_PROMPT_AHEAD="↑"
+# ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}✖"
+# ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}●"
+# ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[red]%}✚"
+# ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
+# ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}⚑"
+# ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"
+
+# # Theming variables for the secondary prompt
+# ZSH_THEME_GIT_PROMPT_SECONDARY_PREFIX=""
+# ZSH_THEME_GIT_PROMPT_SECONDARY_SUFFIX=""
+# ZSH_THEME_GIT_PROMPT_TAGS_SEPARATOR=", "
+# ZSH_THEME_GIT_PROMPT_TAGS_PREFIX="🏷 "
+# ZSH_THEME_GIT_PROMPT_TAGS_SUFFIX=""
+# ZSH_THEME_GIT_PROMPT_TAG="%{$fg_bold[magenta]%}"
+
+# PROMPT
+PROMPT=$'\n┏╸%B%~%b $(gitprompt)\n' \
+&& PROMPT+='┗╸%U${CONDA_DEFAULT_ENV}%u%(!.%F{1}.%F{238})' \
+&& PROMPT+='[%n@%M]%f%(!.#.$) '
