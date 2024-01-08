88206880549e125707fba764fa9a452fc2d975c2
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-01-07 22:13:53 -0800

    Comment out dconf settings

--- dconf/dconf.nix
+++ dconf/dconf.nix
@@ -1,42 +1,42 @@
 {
   settings = {
-    # Swap capslock to escape key
-    "org/gnome/desktop/input-sources" = {
-      xkb-options = ["caps:swapescape"];
-    };
+    # # Swap capslock to escape key
+    # "org/gnome/desktop/input-sources" = {
+    #   xkb-options = ["caps:swapescape"];
+    # };
 
     # Focus on mouse hover
     "org/gnome/desktop/wm/preferences" = {
       focus-mode = "sloppy";
     };
 
-    # Display scale factor to 1.25
-    "org/gnome/desktop/interface" = {
-      text-scaling-factor = 1.25;
-    };
+    # # Display scale factor to 1.25
+    # "org/gnome/desktop/interface" = {
+    #   text-scaling-factor = 1.25;
+    # };
 
-    # File Manager shortcut
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      binding = "<Super>e";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      command = "nautilus";
-    };
+    # # File Manager shortcut
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    #   binding = "<Super>e";
+    # };
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    #   command = "nautilus";
+    # };
 
-    # Clipboard shortcut
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-      binding = "<Super>v";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-      command = "/usr/bin/diodon";
-    };
+    # # Clipboard shortcut
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+    #   binding = "<Super>v";
+    # };
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+    #   command = "/usr/bin/diodon";
+    # };
 
-    # Enable the shortcuts
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-      ];
-    };
+  #   # Enable the shortcuts
+  #   "org/gnome/settings-daemon/plugins/media-keys" = {
+  #     custom-keybindings = [
+  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+  #     ];
+  #   };
   };
 }
